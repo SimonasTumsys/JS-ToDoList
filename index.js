883047949179e85a73fd9f292e0ea602c1a99565
SimonasTumsys <simonas.tumsys@gmail.com>
2022-03-29 20:32:01 +0300

    finished, to be tweaked

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let doneContainer = document.querySelector('.done_container');
 let taskInput = document.querySelector('#task_input')
 let ul = document.createElement('ul');
 doneContainer.insertAdjacentElement('beforebegin', ul);
-let btn = document.querySelector('.btn');
+let btn = document.querySelector('.btn_plus');
+let progressBar = document.querySelector('.progress-bar');
 
 function addTaskToList(){
     if (taskInput.value !== ''){
@@ -16,12 +17,22 @@ function addTaskToList(){
         ul.insertAdjacentHTML('beforeend', task);
     }
     taskInput.value = '';
-    // checkboxEvents()
-    // pencilEvents()
-    // trashCanEvents()
+    //progressBar resets after adding tasks:
+    allTaskCount = document.querySelectorAll('LI').length;
+    progressBar.innerText = `${doneTaskCount} out of ${allTaskCount}`;
+    progressBarValue = (doneTaskCount / allTaskCount)*100
+    progressBar.setAttribute("style", `width:${progressBarValue}%`)
 }
 
+
+
 btn.addEventListener('click', addTaskToList);
+
+//Progress bar label:
+let doneTaskCount = 0;
+allTaskCount = document.querySelectorAll('LI').length;
+progressBar.innerText = `${doneTaskCount} out of ${allTaskCount}`;
+
 
 
 let taskList = document.querySelector('ul');
@@ -29,18 +40,38 @@ taskList.addEventListener('click', function(ev){
     if(ev.target.tagName == 'INPUT'){
         let span = ev.target.parentNode.childNodes[3]
         span.classList.toggle('task_checked')
+        resetProgress()
     }
     else if(ev.target.className == 'bi bi-trash-fill'){
         let li = ev.target.parentNode
         li.remove()
+        resetProgress()
     }
     else if(ev.target.className == 'bi bi-pencil-fill'){
-        
     }
-
-
-
 }, false)
+
+function resetProgress(){
+    doneTaskCount = document.getElementsByClassName('task_checked').length;
+    allTaskCount = document.querySelectorAll('LI').length;
+    progressBar.innerText = `${doneTaskCount} out of ${allTaskCount}`
+    let progressBarValue = (doneTaskCount / allTaskCount)*100
+    if(allTaskCount > 0){
+        progressBar.setAttribute("style", `width:${progressBarValue}%`)
+    } else {progressBar.setAttribute("style", `width:0%`)}
+}
+
+
+let removeCheckedBtn = document.querySelector('.rm_checked');
+
+removeCheckedBtn.addEventListener('click', function(){
+    let checkedTasks = document.querySelectorAll('.task_checked');
+    for(let i of checkedTasks) {
+        i.parentNode.remove()
+    }
+    resetProgress()
+})
+
 
 
 // for(let i = 0; i < taskList.length; i++){
@@ -50,7 +81,7 @@ taskList.addEventListener('click', function(ev){
 //         let span = event.target.nextElementSibling
 //         span.classList.toggle('task_checked')
 //         console.log(span)
-//     })
+//     }) 
 // }
 
 
